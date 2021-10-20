3891d27b157ade432d396b6613ad5cab5878c990
Tnex <>
2021-10-20 13:28:48 +0700

    init

---
+++ lib/dotp.dart
@@ -0,0 +1,10 @@
+///
+/// @project  : jsotp
+/// @author   : Gin (gin.lance.inside@hotmail.com)
+/// @link     : https://github.com/LanceGin/dotp
+/// @Disc     : a node module to generate and verify one-time passwords
+///
+library dotp;
+
+export 'src/hotp.dart';
+export 'src/totp.dart';
